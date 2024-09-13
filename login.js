2392b5c9c4d55c4c6d2f4845ad96a8b7b9a8329b
hamedbelkhiria706 <hamedbelkhiria706@gmail.com>
2024-09-13 13:01:45 +0200

    m

--- login.js
+++ login.js
@@ -21,3 +21,20 @@ app.post("/login", (req, res) => {
 app.listen(3000, () =>
   console.log(`Serveur en cours d'éxécution sur le port 3000`)
 );
+const verifyToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(403).json({ message: "Accès refusé" });
+  }
+  jwt.verify(token, SECRET_KEY, (err, user) => {
+    if (err) {
+      return res.status(401).json({ message: "Token invalide" });
+    }
+    req.user = user;
+    next();
+  });
+};
+//Route protegée
+app.get("/profile", verifyToken, (req, res) => {
+  res.json({ message: `Bienvenue ${req.user.username} ` });
+});
